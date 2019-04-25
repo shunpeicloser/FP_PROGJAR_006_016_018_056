bb9a66c85ed7ce24cea8116e94427cfa518b1fcb
rahadiankp <rahadian.k.putra@gmail.com>
2019-04-25 17:23:25 +0700

    more update

--- socket_backend/client_select.py
+++ socket_backend/client_select.py
@@ -2,6 +2,13 @@ import socket
 import time
 import select
 import sys
+import threading
+
+def threadTest(challenger):
+    print('You are challenged by', challenger, ', accept? [Y/n]')
+    msg = input()
+    print('the msg', msg)
+    return
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('10.151.34.154', 9000))
@@ -20,21 +27,26 @@ try:
             if fd ==  sys.stdin:
                 command = sys.stdin.readline()
 
-                if command.strip('\n') == 'quit':
-                    running = 0
-                    break
-
                 if command:
                     sock.send(command.strip('\n').encode())
                     response = sock.recv(1024)
                     print(response.decode())
+
+                    if command.strip('\n') == 'quit':
+                        running = 0
+                        break
                 #else:
                 #    data = sock.recv(1024)
                 #    print(data.decode())
 
             elif fd == sock:
                 data = sock.recv(1024)
+                alert = data.decode().split()
                 print(data.decode())
+                if alert[0] == 'challenge' and alert[1] == 'invite':
+                    a = threading.Thread(target = threadTest)
+                    a.start()
+                    a.join()
 except KeyboardInterrupt:
     pass
 
