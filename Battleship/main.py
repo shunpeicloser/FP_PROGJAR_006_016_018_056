fb0cc8a3a8afec68f0b70f6cd9214ccac84ec748
miris <tirtajiwanggataufiq@gmail.com>
2019-05-07 16:32:33 +0700

    file scene template

--- Battleship/main.py
+++ Battleship/main.py
@@ -1,14 +1,22 @@
 import pygame
 import scene_login
+import scene_register
+import scene_room
 import scene_battle
 import board
 import control
 
 gamestatus = 1
 ls = scene_login.LoginScene()
+regs = scene_register.RegisterScene()
+rs = scene_room.RoomScene()
 bs = scene_battle.BattleScene(board.Board(10), control.Control())
 while gamestatus > 0:
     if gamestatus == 1:
         gamestatus = ls.run()
+    if gamestatus == 2:
+        gamestatus = regs.run()
+    if gamestatus == 3:
+        gamestatus = rs.run()
     if gamestatus == 4:
         gamestatus = bs.run()
---
+++ Battleship/scene_register.py
@@ -0,0 +1,41 @@
+import pygame
+import utility
+
+class RegisterScene:
+    def __init__(self):
+        self.screen = None
+        self.SCREEN_RESOLUTION = (640, 640)
+        self.resource = {}
+        
+    
+    def loadresource(self):
+        try:
+            self.resource.update({'background': (utility.Utility.loadimage('background.png'), (0, 0))})
+        except:
+            return False
+        return True
+    
+    def drawscreen(self):
+        for item in self.resource:
+            self.screen.blit(self.resource[item][0], self.resource[item][1])
+
+    def startscene(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.SCREEN_RESOLUTION)
+        running = self.loadresource()
+        mousepos = None
+        while running:
+            self.screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    return 0
+
+                mousepos = pygame.mouse.get_pos()
+
+                self.drawscreen()
+                pygame.display.update()
+
+    def run(self):
+        ret = self.startscene()
+        return ret
