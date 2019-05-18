81f669aa71feae1b7bbed338382e7af892340ffb
Aguel Satria Wijaya <aguelsw@gmail.com>
2019-05-18 20:22:54 +0700

    add highscores button

--- Battleship/scene_lobby2.py
+++ Battleship/scene_lobby2.py
@@ -11,6 +11,7 @@ class LobbyScene:
         self.lobby = []
         self.quit_rect = pygame.rect.Rect(40, 550, 150, 60)
         self.refresh_rect = pygame.rect.Rect(210, 550, 230, 60)
+        self.hi_scores_rect = pygame.rect.Rect(450, 550, 180, 60)
         self.boxcolor = pygame.Color('black')
 
         # socket to server
@@ -34,12 +35,16 @@ class LobbyScene:
             self.screen.blit(self.resource[item][0], self.resource[item][1])
         pygame.draw.rect(self.screen, self.boxcolor, self.quit_rect, 8)
         pygame.draw.rect(self.screen, self.boxcolor, self.refresh_rect, 8)
+        pygame.draw.rect(self.screen, self.boxcolor, self.hi_scores_rect, 8)
         quit_surface = pygame.font.Font(None, 64).render('QUIT', True, pygame.Color('white'))
         refresh_surface = pygame.font.Font(None, 64).render('REFRESH', True, pygame.Color('white'))
+        hi_scores_surface = pygame.font.Font(None, 45).render('HI-SCORES', True, pygame.Color('white'))
         quit_fill = pygame.Surface((150, 60))
         refresh_fill = pygame.Surface((230, 60))
+        hi_scores_fill = pygame.Surface((180, 60))
         quit_fill.fill((0, 0, 0))
         refresh_fill.fill((0, 0, 0))
+        hi_scores_fill.fill((0, 0, 0))
 
         num_id = []
         player_name = []
@@ -75,6 +80,8 @@ class LobbyScene:
         self.screen.blit(quit_surface, (self.quit_rect.x + 8, self.quit_rect.y + 8))
         self.screen.blit(refresh_fill, (self.refresh_rect.x, self.refresh_rect.y))
         self.screen.blit(refresh_surface, (self.refresh_rect.x + 8, self.refresh_rect.y + 8))
+        self.screen.blit(hi_scores_fill, (self.hi_scores_rect.x, self.hi_scores_rect.y))
+        self.screen.blit(hi_scores_surface, (self.hi_scores_rect.x + 8, self.hi_scores_rect.y + 8))
 
     def getPlayers(self):
         # send request players listing
@@ -114,6 +121,10 @@ class LobbyScene:
                 if event.type == pygame.MOUSEBUTTONUP:
                     if self.quit_rect.collidepoint(mousepos):
                         return 0
+                    if self.hi_scores_rect.collidepoint(mousepos):
+                        # refresh event here
+                        print("go to highscores")
+                        return 5
                     if self.refresh_rect.collidepoint(mousepos):
                         # refresh event here
                         print("refreshed")
